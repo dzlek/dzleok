b31785d6ecce91e52c23e408647fd4e39303a554
dzlek <6894255@gmail.com>
2025-02-05 21:52:45 +0300

    feat: added husky

--- eslint.config.js
+++ eslint.config.js
@@ -27,6 +27,7 @@ export default tseslint.config(
       'react-compiler': reactCompiler,
     },
     rules: {
+      'linebreak-style': ['error', 'unix'],
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
