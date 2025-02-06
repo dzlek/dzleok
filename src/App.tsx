144e0ffec8d5e20928da752eaa8cd57f291c99ff
dzlek <6894255@gmail.com>
2025-02-06 12:35:26 +0300

    feat: add favicon

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from './components/Header/Header';
 
 function App() {
   function handleError() {
-    throw new Error('Something went wrong');
+    throw new Error('wrong');
   }
 
   return (
