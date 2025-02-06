f1ad513c2bc7ba732de9600700aeb06db10d75e5
dzlek <6894255@gmail.com>
2025-02-06 16:52:00 +0300

    feat: add sass styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,15 @@ import './App.css';
 import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary';
 import Header from './components/Header/Header';
 import Results from './components/Results/Results';
+import ThrowError from './components/ThrowError/ThrowError';
 
 function App() {
-  function handleError() {
-    throw new Error('wrong');
-  }
-
   return (
     <div className="app">
-      <ErrorBoundary fallback={<h1>Something went wrong</h1>}>
+      <ErrorBoundary fallback={<h1>Something went wrong. Reload the page</h1>}>
+        <ThrowError />
         <Header />
         <Results />
-        <button onClick={handleError}>Error Button</button>
       </ErrorBoundary>
     </div>
   );
